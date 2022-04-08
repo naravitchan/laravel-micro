66b8ba6f18073ae2fad9f2a21a8e70afeb811c1c
naravich@enres.co <seagame@MacBook-Pro-3.local>
2022-04-08 11:49:43 +0700

    add first

---
+++ react-ambassador/next-checkout/constants.ts
@@ -0,0 +1,4 @@
+export default {
+    endpoint: 'http://localhost:8000/api/checkout',
+    stripe_key: 'pk_test_51KknTjGnjieaObT257y7XTdvA5jaLXzPf6nC7XbD6mvGE41sPpUmY2lmrS85AbBBcIlervTWlg4QQKcNbbBu2HMQ00hYRwO6Al'
+}
