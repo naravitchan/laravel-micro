e4fbb3ff5a8d3796a4b0380e962a8c2abaad30df
naravich@enres.co <seagame@MacBook-Pro-3.local>
2022-04-08 11:37:26 +0700

    first

---
+++ email/app/Listeners/NotifyAdminListener.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Listeners;
+ueue;
+use Illuminate\Queue\InteractsWithQ
+use Illuminate\Mail\Message;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class NotifyAdminListener
+{
+    // public function handle(OrderCompletedEvent $event)
+    // {
+    //     $order = $event->order;
+
+    //     \Mail::send('admin', ['order' => $order], function (Message $message) {
+    //         $message->subject('An Order has been completed');
+    //         $message->to('admin@admin.com');
+    //     });
+    // }
+}
